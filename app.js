6f6a6d31fc9516626dc4f1f16070ebd2f7f6194f
Warren <tamagrijr@gmail.com>
2021-03-30 23:17:16 -0600

    more testing cors

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const corsOptions = {
   }
 }
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000", 'https://tamagrijr.github.io/dnsFrontend/', 'https://tamagrijr.github.io');
+  res.setHeader('Access-Control-Allow-Origin',  'https://tamagrijr.github.io/');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 app.use(cors(corsOptions));
